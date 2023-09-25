045797f9c5a7bfc1d03ddff47be0c2a231886f8d
INoorik <ajnurabuzarov8@gmail.com>
2023-09-25 06:41:54 +0300

    ast.h created

---
+++ ast.h
@@ -0,0 +1,30 @@
+#ifndef AST
+#define AST
+
+#include "parser.h"
+
+#define MAX_AST_NODE_CHILDS 5
+
+typedef enum{
+	AST_NOT, 
+	AST_AND,
+	AST_OR,
+	AST_EQU,
+	AST_CONSTANT,
+	AST_VARIABLE,
+	AST_ASSIGNMENT,
+	AST_READ,
+	AST_WRITE,
+	AST_WRITE_CR,
+	AST_BRANCH
+} Ast_node_type;
+
+typedef struct Ast_node{
+	Ast_node_type type;
+	int data;
+	struct Ast_node childs[MAX_AST_NODE_CHILDS];
+} Ast_node;
+
+Ast_node *build_ast(Parsing_tree_node *tree, int *identifiers_cnt);
+
+#endif

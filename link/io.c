1dc68b2ac4fa51e0885df6e2aeee9b7a9b851fac
INoorik <ajnurabuzarov8@gmail.com>
2023-09-28 19:54:51 +0300

    io c file added

---
+++ link/io.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void write_cr()
+{
+	printf("\n");
+}
+
+void write(int ident, char *array)
+{
+	int byte = ident/8;
+	int bit = ident % 8;
+	if(array[byte] & (1 << bit))
+	{
+		printf("1");
+	}else{
+		printf("0");
+	}
+}
+
+void read(int ident, char *array)
+{
+	int byte = ident/8;
+	int bit = ident % 8;
+	int value;
+	printf("? ");
+	scanf("%d", &value);
+	if(value == 0)
+	{
+		array[byte] &= (1 << bit) ^ 0xff;
+	}else if(value == 1){
+		array[byte] |= (1 << bit);
+	}else{
+		printf("Value must be 1 or 0\n");
+		read(ident, array);
+	}
+}
